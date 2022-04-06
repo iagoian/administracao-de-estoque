049a355a5d9bc50ec62493bc767dc7e39f300688
iagoian <iagoian23@hotmail.com>
2022-04-06 16:06:25 -0300

    criação do formulário de cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'cadastrar', component: CadastrarComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: 
+    [RouterModule.forRoot(routes)],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
